066168d28d595eed2a17c97c19e1b958309f2f66
Francisco Sousa <onics.aulas@gmail.com>
2024-06-26 16:33:50 -0300

    Criando schemas e models - Entidade Categoria e Centro de treinamento

--- workout_api/atleta/models.py
+++ workout_api/atleta/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from sqlalchemy import Integer, String, Float
-from sqlalchemy.orm import DateTime, Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from workout_api.contrib.models import BaseModel
 
 class AtletaModel(BaseModel):
@@ -14,4 +14,16 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    categoria: Mapped['CategoriaModel'] = relationship(back_populates='atleta')
+    categoria_id = Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'))
+
+    centro_treinamento: Mapped['CentroTreinamentoModel'] = relationship(back_populates='atleta')
+    centro_treinamento.id = Mapped[int] = mapped_column(ForeignKey('centros_treinamento.pk_id'))
+
+    
+
+
+
+
 
